ac87a8a79d2bb469dd2935b3b3d807ab342c4595
RalfiSlask <nilssonnarki@gmail.com>
2024-12-26 13:45:03 +0100

    add email transporter using nodemailer

---
+++ src/app/lib/nodemailer.ts
@@ -0,0 +1,9 @@
+import * as nodemailer from 'nodemailer';
+
+export const emailTransporter = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
