d020cdd3c5ea0b6bb5223b584123b5fc7ba366f4
RalfiSlask <nilssonnarki@gmail.com>
2024-12-26 12:49:02 +0100

    Create validation schemas for auth forms using the Yup library

---
+++ src/app/utils/validationSchemas.ts
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+export const registrationValidationSchema = Yup.object({
+  name: Yup.string()
+    .min(2, 'Namnet måste vara åtminstone 2 bokstäver')
+    .required('Namn är obligatoriskt'),
+  email: Yup.string()
+    .email('Felaktigt e-mail format')
+    .required('E-mail är obligatoriskt'),
+  password: Yup.string()
+    .min(2, 'Lösenord måste vara åtminstone 8 karaktärer')
+    .required('Lösenord är obligatoriskt'),
+});
+
+export const signInValidationSchema = Yup.object({
+  email: Yup.string()
+    .email('Felaktigt e-mail format')
+    .required('E-mail är obligatoriskt'),
+  password: Yup.string()
+    .min(2, 'Lösenord måste vara åtminstone 8 karaktärer')
+    .required('Lösenord är obligatoriskt'),
+});
