975303d193ee065df4f0bb19eac93dafb6bafecc
RalfiSlask <nilssonnarki@gmail.com>
2024-12-27 09:33:27 +0100

    change header so that we now have separate subheaders for login and navigation

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import menu from '../../data/menu.json';
-import MenuItem from './MenuItem';
+import LoginNavigation from './LoginNavigation';
+import Navbar from './Navbar';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+  const pathname = usePathname();
+  const isAccountPage = pathname.startsWith('/konto');
+
+  if (isAccountPage) return null;
+
   return (
-    <header className="bg-slate-300 h-[80px] w-screen flex px-4 items-center justify-end text-black">
-      <div className="w-full flex justify-end">
-        {menu.menuItems.map((menuItem) => {
-          return <MenuItem key={menuItem.id} menuItem={menuItem} />;
-        })}
-      </div>
+    <header className="bg-slate-300 h-40 w-screen flex flex-col justify-between items-end text-black">
+      <LoginNavigation />
+      <Navbar />
     </header>
   );
 };
