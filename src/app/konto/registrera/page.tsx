ad47c91e85bbbb1f0dc7a623f0b65b759c46a5b1
RalfiSlask <nilssonnarki@gmail.com>
2024-12-27 09:36:51 +0100

    add template page for the registration page

---
+++ src/app/konto/registrera/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import RegisterForm from '@/app/components/pages/auth/register/RegisterForm';
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <h2>Registrera</h2>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegistrationPage;
