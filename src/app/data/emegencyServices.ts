91f919a299579ce98d7cfe77eea02e1127802a18
RalfiSlask <nilssonnarki@gmail.com>
2025-01-02 16:47:50 +0100

    add data for emergency services, phone numbers

---
+++ src/app/data/emegencyServices.ts
@@ -0,0 +1,38 @@
+export const emergencyServices = [
+  {
+    id: 1,
+    service: 'Självmordslinjen',
+    number: '90101',
+    description: 'Öppet dygnet runt',
+  },
+  {
+    id: 2,
+    service: 'Mind Självmordslinje',
+    number: '020-22 00 60',
+    description: 'För stöd och råd',
+  },
+  {
+    id: 3,
+    service: 'Jourhavande Medmänniska',
+    number: '08-702 16 80',
+    description: 'Öppet kvällar och nätter',
+  },
+  {
+    id: 4,
+    service: 'Bris',
+    number: '116 111',
+    description: 'För barn och unga',
+  },
+  {
+    id: 5,
+    service: 'Vårdguiden 1177',
+    number: '1177',
+    description: 'Råd och stöd i hela Sverige',
+  },
+  {
+    id: 6,
+    service: 'Läkare Utan Gränser',
+    number: '08-55 60 98 00',
+    description: 'Kontakt för humanitärt stöd',
+  },
+];
