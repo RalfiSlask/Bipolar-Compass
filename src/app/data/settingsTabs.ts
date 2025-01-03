d43f652fb91de2e07d3a862e5673a533c1bd0d75
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 09:20:49 +0100

    add data for settingstabs

---
+++ src/app/data/settingsTabs.ts
@@ -0,0 +1,7 @@
+export const settingsTabs = [
+  { id: 'profile', label: 'Profil' },
+  { id: 'security', label: 'Säkerhet' },
+  { id: 'relatives', label: 'Anhöriga' },
+  { id: 'notifications', label: 'Notifikationer' },
+  { id: 'medicines', label: 'Mediciner' },
+];
