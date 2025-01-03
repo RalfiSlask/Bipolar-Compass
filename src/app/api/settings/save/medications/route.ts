e13cbfbe13f26a4fbe5000edd55e163e65c00fdd
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 14:36:09 +0100

    add route for saving the users medications

---
+++ src/app/api/settings/save/medications/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+import { getCollection } from '@/app/utils/databaseUtils';
+import { NextRequest } from 'next/server';
+
+export const PUT = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const { medications, email } = await req.json();
+    const collection = await getCollection('thesis', 'users');
+    const user = await collection.findOne({ email: email });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const updatedUser = { ...user, profile: { ...user.profile, medications } };
+
+    await collection.updateOne({ email: email }, { $set: updatedUser });
+    return NextResponse.json({ message: 'Medicines saved' });
+  } catch (err) {
+    console.error('could not save medications: ', err);
+    return NextResponse.json(
+      { error: 'could not save medications' },
+      { status: 500 }
+    );
+  }
+};
