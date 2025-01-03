f3430091f5745faceb602487627cc47582d3c042
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 09:21:12 +0100

    add data for medicines and their categories

---
+++ src/app/data/medications.ts
@@ -0,0 +1,20 @@
+export const medicineCategories = {
+  moodStabilizers: [
+    { value: 'lithium', label: 'Lithium' },
+    { value: 'lamotrigine', label: 'Lamotrigin' },
+    { value: 'valproate', label: 'Valproat' },
+    { value: 'carbamazepine', label: 'Karbamazepin' },
+  ],
+  antipsychotics: [
+    { value: 'olanzapine', label: 'Olanzapin (Zyprexa)' },
+    { value: 'quetiapine', label: 'Quetiapin (Seroquel)' },
+    { value: 'aripiprazole', label: 'Aripiprazol (Abilify)' },
+    { value: 'risperidone', label: 'Risperidon (Risperdal)' },
+  ],
+  antidepressants: [
+    { value: 'sertraline', label: 'Sertralin (Zoloft)' },
+    { value: 'fluoxetine', label: 'Fluoxetin (Prozac)' },
+    { value: 'venlafaxine', label: 'Venlafaxin (Effexor)' },
+    { value: 'bupropion', label: 'Bupropion (Wellbutrin)' },
+  ],
+};
