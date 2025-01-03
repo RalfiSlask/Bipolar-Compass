ef3af3f674ec6596a898d01c559e85977cfbc914
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 14:35:11 +0100

    add route for adding relatives in settings

---
+++ src/app/api/settings/save/relatives/route.ts
@@ -0,0 +1,29 @@
+import { getCollection } from '@/app/utils/databaseUtils';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const PUT = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const { relatives, email } = await req.json();
+
+    const collection = await getCollection('thesis', 'users');
+
+    const user = await collection.findOne({ email: email });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const updatedUser = { ...user, settings: { ...user.settings, relatives } };
+
+    await collection.updateOne({ email: email }, { $set: updatedUser });
+
+    console.log('relatives: ', relatives);
+    return NextResponse.json({ message: 'Relatives saved' });
+  } catch (err) {
+    console.error('could not save relatives: ', err);
+    return NextResponse.json(
+      { error: 'could not save relatives' },
+      { status: 500 }
+    );
+  }
+};
