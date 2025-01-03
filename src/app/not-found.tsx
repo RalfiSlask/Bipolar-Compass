f2981d4920b938d966e8cdd617548133473902b0
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 09:21:32 +0100

    update colors according to updated names

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import LinkIcon from './components/shared/LinkIcon';
 const NotFound = () => {
   return (
     <section className="flex flex-col items-start gap-6 pt-20">
-      <h2 className="h-xl text-green-lightest mb-4 text-center sm:text-left">
+      <h2 className="h-xl text-primary-dark mb-4 text-center sm:text-left">
         Sidan kunde inte hittas
       </h2>
       <p className="max-w-[600px]">
@@ -14,16 +14,14 @@ const NotFound = () => {
       </p>
       <div className="w-full flex flex-col gap-6">
         <div className="flex flex-col gap-2">
-          <h3 className="h-xs text-green-lightest">Vad det kan bero på</h3>
+          <h3 className="h-xs text-primary-dark">Vad det kan bero på</h3>
           <ul className="w-full">
             <li>- Att du skrivit in en eller följt en länk som ej fungerar</li>
             <li>- Navigerat till en sida som ej längre existerar</li>
           </ul>
         </div>
         <div className="flex flex-col gap-2">
-          <h3 className="h-xs text-green-lightest">
-            För att fixa detta kan du
-          </h3>
+          <h3 className="h-xs text-primary-dark">För att fixa detta kan du</h3>
           <ul className="w-full">
             <li>- Navigera till sidor via menyn</li>
             <li>- Ändra webbadressen</li>
@@ -39,7 +37,7 @@ const NotFound = () => {
           Om du ej hittar det du söker var vänlig att,
         </span>
         <Link
-          className="underline hover:text-green-light inline-flex items-center"
+          className="underline hover:text-primary-dark inline-flex items-center"
           href="/om-oss/kontakt"
         >
           <LinkIcon />
