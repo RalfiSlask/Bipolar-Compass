b35522204b279162a254866521d31e89b4dbabbb
RalfiSlask <nilssonnarki@gmail.com>
2025-01-03 14:36:29 +0100

    add route for updating the users password

---
+++ src/app/api/settings/save/security/password/route.ts
@@ -0,0 +1,54 @@
+import { getCollection } from '@/app/utils/databaseUtils';
+import bcryptjs from 'bcryptjs';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function PUT(req: NextRequest) {
+  try {
+    const { currentPassword, newPassword, email } = await req.json();
+
+    const collection = await getCollection('thesis', 'users');
+
+    const user = await collection.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: 'Användaren hittades inte' },
+        { status: 404 }
+      );
+    }
+
+    const isValidPassword = await bcryptjs.compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isValidPassword) {
+      return NextResponse.json(
+        { message: 'Felaktigt nuvarande lösenord' },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcryptjs.hash(newPassword, 12);
+
+    await collection.updateOne(
+      { email },
+      {
+        $set: {
+          password: hashedPassword,
+        },
+      }
+    );
+
+    return NextResponse.json(
+      { message: 'Lösenordet har uppdaterats' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Password update error:', error);
+    return NextResponse.json(
+      { message: 'Ett fel uppstod vid uppdatering av lösenord' },
+      { status: 500 }
+    );
+  }
+}
