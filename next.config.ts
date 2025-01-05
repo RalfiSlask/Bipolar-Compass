ec35306e90f7e426a99b2242ddae58a33bd24fa3
RalfiSlask <nilssonnarki@gmail.com>
2025-01-05 08:29:05 +0100

    add image domain for the podcast api

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['m.media-amazon.com'],
+    domains: ['m.media-amazon.com', 'cdn-images-3.listennotes.com'],
   },
   /* config options here */
 };
