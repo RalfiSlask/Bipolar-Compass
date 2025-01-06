164035dc9bbddcb79f3712fcef8389c350698bcb
RalfiSlask <nilssonnarki@gmail.com>
2025-01-06 16:07:41 +0100

    change so we now add mood tracking base values when re register a new user

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,66 @@
+import { User } from '@/app/models/User';
+import { sendVerificationEmail } from '@/app/utils/emailUtils';
+import bcrypt from 'bcryptjs';
+import { NextRequest, NextResponse } from 'next/server';
+import { getCollection } from '../../utils/databaseUtils';
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  try {
+    const { name, email, password } = await req.json();
+
+    const usersCollection = await getCollection('thesis', 'users');
+    const moodTrackerCollection = await getCollection(
+      'thesis',
+      'mood_tracker_weeks'
+    );
+
+    // Check if email already exists
+    const emailAlreadyUsed = await usersCollection.findOne({ email });
+    if (emailAlreadyUsed) {
+      console.log('Email is already in use');
+      return NextResponse.json(
+        { error: 'E-posten är redan använd.' },
+        { status: 400 }
+      );
+    }
+
+    // Create new user
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User(name, email, encryptedPassword);
+    const serializedUser = newUser.toJSON();
+
+    try {
+      // Save user first
+      const userResult = await usersCollection.insertOne(serializedUser);
+      const userId = userResult.insertedId.toString();
+
+      // Update moodtracker with user-ID and save it
+      newUser.moodtracker.user_id = userId;
+      await moodTrackerCollection.insertOne(newUser.moodtracker);
+
+      // Send verification email
+      await sendVerificationEmail({
+        verificationToken: newUser.verificationToken!,
+        email: newUser.email,
+      });
+
+      return NextResponse.json(
+        { message: 'User created, verification link sent' },
+        { status: 201 }
+      );
+    } catch (error) {
+      console.error('Error during user creation:', error);
+
+      return NextResponse.json(
+        { error: 'Could not create user' },
+        { status: 500 }
+      );
+    }
+  } catch (error) {
+    console.error('An error occurred during user creation:', error);
+    return NextResponse.json(
+      { error: 'Error when creating user' },
+      { status: 500 }
+    );
+  }
+};
