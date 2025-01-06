2a2578f452a07b5b049568b5a3b7e040447d4c9b
RalfiSlask <nilssonnarki@gmail.com>
2025-01-06 10:11:21 +0100

    add image domain for spotify

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['m.media-amazon.com', 'cdn-images-3.listennotes.com'],
+    domains: [
+      'm.media-amazon.com',
+      'cdn-images-3.listennotes.com',
+      'i.scdn.co',
+    ],
   },
   /* config options here */
 };
