2c5c92c91eda55493bbd96057ef3552890e3d1fe
RalfiSlask <nilssonnarki@gmail.com>
2025-01-06 16:18:43 +0100

    add data for some spotify playlists

---
+++ src/app/data/playlists.ts
@@ -0,0 +1,114 @@
+export const PLAYLIST_BIPOLAR = [
+  {
+    id: '5mEUYreFXXc0MvRMxOXp95',
+    title: 'Songs about life with bipolar',
+  },
+];
+
+export const PLAYLIST_SLEEP = [
+  {
+    id: '37i9dQZF1DWZd79rJ6a7lp',
+    title: 'Sleep',
+  },
+  {
+    id: '37i9dQZF1DX8Uebhn9wzrS',
+    title: 'Chill Lofi Study Beats',
+  },
+  {
+    id: '37i9dQZF1DWYcDQ1hSjOpY',
+    title: 'Deep Sleep',
+  },
+  {
+    id: '37i9dQZF1DX4DgyXDzlbNz',
+    title: 'Peaceful Piano',
+  },
+  {
+    id: '37i9dQZF1DWXzR2dg7Pcy4',
+    title: 'Night Rain',
+  },
+  {
+    id: '37i9dQZF1DX2PQDq3PdrHQ',
+    title: 'Nature Sounds',
+  },
+];
+
+export const PLAYLIST_MEDITATION = [
+  {
+    id: '37i9dQZF1DX9uKNf5jGX6m',
+    title: 'Meditation Music',
+  },
+  {
+    id: '37i9dQZF1DWZqd5JICZI0u',
+    title: 'Peaceful Meditation',
+  },
+  {
+    id: '37i9dQZF1DX5Lm1ZiObdc3',
+    title: 'Lo-Fi Beats',
+  },
+  {
+    id: '37i9dQZF1DWUvQoIOFMFUT',
+    title: 'Ambient Relaxation',
+  },
+  {
+    id: '37i9dQZF1DX3Ogo9pFvBkY',
+    title: 'Ambient Chill',
+  },
+  {
+    id: '37i9dQZF1DX9sIqqvKsjG8',
+    title: 'White Noise',
+  },
+];
+
+export const PLAYLIST_ENERGY = [
+  {
+    id: '37i9dQZF1DX9XIFQuFvzM4',
+    title: 'Morning Motivation',
+  },
+  {
+    id: '37i9dQZF1DX0HRj9P7NxeE',
+    title: 'Feelgood Pop',
+  },
+  {
+    id: '37i9dQZF1DX1H4LbvY4OJi',
+    title: 'Happy Hits',
+  },
+  {
+    id: '37i9dQZF1DX3rxVfibe1L0',
+    title: 'Mood Booster',
+  },
+  {
+    id: '37i9dQZF1DX84kJlLdo9DT',
+    title: 'Happy Beats',
+  },
+  {
+    id: '37i9dQZF1DXdPec7aLTmlC',
+    title: 'Dance Party',
+  },
+];
+
+export const PLAYLIST_CALM = [
+  {
+    id: '37i9dQZF1DX3Ogo9pFvBkY',
+    title: 'Ambient Chill',
+  },
+  {
+    id: '37i9dQZF1DWXe9gFZP0gtP',
+    title: 'Acoustic Calm',
+  },
+  {
+    id: '37i9dQZF1DWYMroOc5KTTh',
+    title: 'Calming Acoustic',
+  },
+  {
+    id: '37i9dQZF1DX4DgyXDzlbNz',
+    title: 'Peaceful Piano',
+  },
+  {
+    id: '37i9dQZF1DWZ0OzPeadl0h',
+    title: 'Grounding Music',
+  },
+  {
+    id: '37i9dQZF1DWUvQoIOFMFUT',
+    title: 'Ambient Relaxation',
+  },
+];
