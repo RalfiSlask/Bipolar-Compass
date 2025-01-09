fdda538a12a045b7bc21143cb36230b55a55e965
RalfiSlask <nilssonnarki@gmail.com>
2025-01-09 12:57:22 +0100

    add component icons for having on diagnoses page

---
+++ src/app/components/shared/icons/SpinningArrowsIcon.tsx
@@ -0,0 +1,20 @@
+export const SpinningArrowsIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      width="512px"
+      height="512px"
+      viewBox="0 0 512 512"
+      enableBackground="new 0 0 512 512"
+      className={className}
+      fill="currentColor"
+    >
+      <path
+        fill="currentColor"
+        d="M335.365,256.15c0,86.84-70.65,157.48-157.48,157.48h-50.02v75.9l-95.89-95.9l95.89-95.89v75.89h50.02  c64.77,0,117.48-52.7,117.48-117.48H335.365z M384.135,214.26l95.89-95.89l-95.89-95.9v75.9h-51.05  c-86.92,0-157.64,70.71-157.64,157.63h40c0-64.86,52.77-117.63,117.64-117.63h51.05V214.26z"
+      />
+    </svg>
+  );
+};
