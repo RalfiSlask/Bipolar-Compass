0da4c4a4e678170440457dee5852524b0a0c8b04
RalfiSlask <nilssonnarki@gmail.com>
2025-01-13 10:42:53 +0100

    config: update tailwind config with new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,28 +9,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        // Tertiary colors (yellow tones)
-        'tertiary-light': '#fcd961', // Light yellow
-        'tertiary-medium': '#f9b732', // Medium yellow
-        'tertiary-dark': '#f4c31f', // Dark yellow
+        'primary-accent': '#46737c',
+        'primary-light': '#eff7f7',
+        'primary-medium': '#659598',
+        'primary-border': '#a1b8bd',
+        'primary-dark': '#19505b',
 
-        // Primary colors (green tones)
-        'primary-light': '#d6e9d8', // Light green
-        'primary-medium': '#045346', // Medium green
-        'primary-dark': '#13404f', // Dark green
+        'secondary-light': '#D0E8E6',
+        'secondary-dark': '#25424e',
 
-        // Secondary colors (teal tones)
-        'secondary-light': '#2ebe7e', // Light teal
-        'secondary-medium': '#257f69', // Medium teal
-        'secondary-dark': '#1e594f', // Dark teal
+        'tertiary-light': '#fffacd',
+        'tertiary-dark': '#FFD700',
 
-        // Quaternary colors (orange tones)
-        'quaternary-light': '#f5be99', // Light orange
-        'quaternary-medium': '#e58748', // Medium orange
-        'quaternary-dark': '#c5672d', // Dark orange
+        'light': '#b0d9d566',
+        'dark': '#202020',
 
-        // Neutral colors
-        beige: '#f8f4ed', // Neutral beige
         'footer-color': '#1a1f20', // Footer dark gray
       },
     },
