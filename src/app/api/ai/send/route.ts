555692761ec1e6a292986fff5c3d34ecb297be07
RalfiSlask <nilssonnarki@gmail.com>
2025-01-13 10:46:12 +0100

    feat: add route for handling the post action for ai-chat from the client and the answer back

---
+++ src/app/api/ai/send/route.ts
@@ -0,0 +1,85 @@
+import { AiConversation, ConversationLog } from '@/app/models/Ai';
+import { IConversationLog } from '@/app/types/ai';
+import { getCollection } from '@/app/utils/databaseUtils';
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const { message, userId } = await req.json();
+    const AiCollection = await getCollection('thesis', 'conversations');
+
+    let conversation = await AiCollection.findOne({
+      user_id: userId.toString(),
+    });
+
+    if (!conversation) {
+      console.log('Creating new conversation for userId:', userId);
+      const newConversation = new AiConversation([]);
+      newConversation.user_id = userId.toString();
+      const result = await AiCollection.insertOne(newConversation);
+      conversation = {
+        ...newConversation,
+        _id: result.insertedId,
+      };
+    }
+    const botCollection = await getCollection('thesis', 'ai_assistant');
+    const botData = await botCollection.findOne({
+      name: 'Mental Health Assistant',
+    });
+    const systemPrompt = botData?.role || 'Du är en expert på bipolaritet...';
+
+    const messages = [
+      { role: 'system', content: systemPrompt },
+      ...conversation.conversation_log.map((log: IConversationLog) => ({
+        role: log.role,
+        content: log.content,
+      })),
+      { role: 'user', content: message },
+    ];
+
+    const chatResponse = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+    });
+
+    const assistantMessage = chatResponse.choices[0].message.content;
+    if (!assistantMessage) {
+      return NextResponse.json({
+        error: 'No assistant message received from open ai',
+      });
+    }
+
+    const updatedLogs = [
+      ...conversation.conversation_log,
+      new ConversationLog('user', message),
+      new ConversationLog('assistant', assistantMessage),
+    ];
+    console.log('Updated logs:', updatedLogs);
+
+    await AiCollection.updateOne(
+      { user_id: userId.toString() },
+      {
+        $set: {
+          conversation_log: updatedLogs,
+          updated_at: new Date().toISOString(),
+        },
+      }
+    );
+
+    return NextResponse.json({
+      answer: assistantMessage,
+      context: updatedLogs,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+};
