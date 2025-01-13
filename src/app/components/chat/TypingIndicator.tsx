72f5ca7bcf5378ab6bd9b1c61018e9e30ab606ca
RalfiSlask <nilssonnarki@gmail.com>
2025-01-13 10:44:17 +0100

    feat: add typing indicator for when ai bot answer is loading

---
+++ src/app/components/chat/TypingIndicator.tsx
@@ -0,0 +1,11 @@
+const TypingIndicator = () => {
+  return (
+    <div className="typing-indicator ml-0 sm:ml-12 w-14 h-14 ">
+      <span className="typing-indicator-dot"></span>
+      <span className="typing-indicator-dot"></span>
+      <span className="typing-indicator-dot"></span>
+    </div>
+  );
+};
+
+export default TypingIndicator;
