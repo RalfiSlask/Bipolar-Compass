5d236ba309458ea25276cf46257dab0609c2ba08
RalfiSlask <nilssonnarki@gmail.com>
2025-01-15 17:41:48 +0100

    feat: change prod url

--- src/app/utils/emailUtils.ts
+++ src/app/utils/emailUtils.ts
@@ -1,4 +1,5 @@
 import EmailTemplate from '../components/EmailTemplate';
+import PasswordTemplate from '../components/PasswordTemplate';
 import { emailTransporter } from '../lib/nodemailer';
 
 export const getVerificationLink = (token: string): string => {
@@ -7,7 +8,7 @@ export const getVerificationLink = (token: string): string => {
   if (process.env.NODE_ENV === 'development') {
     baseUrl = process.env.NEXT_LOCAL_URL || 'http://localhost:3000';
   } else if (process.env.NODE_ENV === 'production') {
-    baseUrl = process.env.NEXT_PROD_URL || 'https://example.com';
+    baseUrl = process.env.NEXT_PROD_URL || 'https://bipolärkompassen.se';
   } else {
     console.warn(
       'NODE_ENV is not correctly defined. We use fallback local url.'
@@ -45,5 +46,50 @@ export const sendVerificationEmail = async ({
       err
     );
     throw new Error('Verification email could not be sent');
+  }
+};
+
+const getResetLink = (token: string): string => {
+  let baseUrl: string;
+
+  if (process.env.NODE_ENV === 'development') {
+    baseUrl = process.env.NEXT_LOCAL_URL || 'http://localhost:3000';
+  } else if (process.env.NODE_ENV === 'production') {
+    baseUrl = process.env.NEXT_PROD_URL || 'https://bipolärkompassen.se';
+  } else {
+    console.warn(
+      'NODE_ENV is not correctly defined. We use fallback local url.'
+    );
+    baseUrl = 'http://localhost:3000';
+  }
+
+  return `${baseUrl}/konto/aterstall-losenord?token=${token}`;
+};
+
+export const sendForgotPasswordEmail = async ({
+  email,
+  resetToken,
+}: {
+  email: string;
+  resetToken: string;
+}) => {
+  const resetLink = getResetLink(resetToken);
+  const emailHtml = PasswordTemplate({ resetLink });
+
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Återställ ditt lösenord',
+      html: emailHtml,
+    };
+
+    await emailTransporter.sendMail(mailOptions);
+  } catch (err) {
+    console.error(
+      'Something went wrong when trying to send forgot password email: ',
+      err
+    );
+    throw new Error('Forgot password email could not be sent');
   }
 };
