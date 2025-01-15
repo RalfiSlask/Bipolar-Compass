74d4b49d4081ce053d64b1856524b715abcd4c19
RalfiSlask <nilssonnarki@gmail.com>
2025-01-15 17:42:26 +0100

    feat: move and improve how the sidebar logic functions

--- src/app/min-sida/layout.tsx
+++ src/app/min-sida/layout.tsx
@@ -1,22 +1,35 @@
 'use client';
 
 import DashboardSidebar from '@/app/components/dashboard/DashboardSidebar';
+import { ICustomSession } from '@/app/types/authoptions';
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function MyPageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { data: session } = useSession();
+  const { data: session } = useSession() as { data: ICustomSession | null };
   const email = session?.user?.email;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   return (
-    <div className="flex h-screen">
-      <DashboardSidebar email={email || ''} />
-      <div className="dashboard-content w-full">
+    <div className="flex h-full w-full">
+      <DashboardSidebar
+        email={email || ''}
+        isOpen={isSidebarOpen}
+        setIsOpen={setIsSidebarOpen}
+      />
+      <div
+        className={`dashboard-content ${
+          isSidebarOpen ? 'content--open' : 'content--closed'
+        } w-full`}
+      >
         {children}
       </div>
+      <Toaster position="bottom-right" />
     </div>
   );
 }
