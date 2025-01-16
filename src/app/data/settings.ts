7a045b851652b8acd815cfa9371762819ca9f8cb
RalfiSlask <nilssonnarki@gmail.com>
2025-01-16 17:47:40 +0100

    data: add profile options for dropdowns regarding gender / diagnosis

---
+++ src/app/data/settings.ts
@@ -0,0 +1,36 @@
+export const GENDER_OPTIONS = [
+  { value: 'Man', label: 'Man' },
+  { value: 'Kvinna', label: 'Kvinna' },
+  { value: 'Annat', label: 'Annat' },
+  { value: 'Vill ej ange', label: 'Vill ej ange' },
+];
+
+export const DIAGNOSIS_OPTIONS = [
+  { value: 'Ej valt', label: 'Ej valt' },
+  {
+    value: 'Bipolär typ 1',
+    label: 'Bipolär typ 1',
+  },
+  {
+    value: 'Bipolär typ 2',
+    label: 'Bipolär typ 2',
+  },
+  { value: 'Cyklotymi', label: 'Cyklotymi' },
+  {
+    value: 'Bipolär ospecificerad',
+    label: 'Bipolär ospecificerad',
+  },
+  {
+    value: 'Blandepisoder',
+    label: 'Blandepisoder',
+  },
+  {
+    value: 'Snabbcyklande',
+    label: 'Snabbcyklande',
+  },
+  {
+    value: 'Säsongsbunden',
+    label: 'Säsongsbunden',
+  },
+  { value: 'Vill ej ange', label: 'Vill ej ange' },
+];
