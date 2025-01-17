1ef3c1cf6afab0b698a0630b28120a61df7dc20d
RalfiSlask <nilssonnarki@gmail.com>
2025-01-17 14:49:32 +0100

    utils: add a helper util for removing html elements from cells when we delete an entry

--- src/app/utils/diaryUtils.ts
+++ src/app/utils/diaryUtils.ts
@@ -1,10 +1,24 @@
 export const getMoodEmoji = (mood?: string) => {
-    const moodMap: { [key: string]: string } = {
-      glad: '😊',
-      neutral: '😐',
-      ledsen: '😢',
-      energisk: '⚡',
-      trött: '😴',
-    };
-    return mood ? moodMap[mood] : '';
+  const moodMap: { [key: string]: string } = {
+    glad: '😊',
+    neutral: '😐',
+    ledsen: '😢',
+    energisk: '⚡',
+    trött: '😴',
   };
+  return mood ? moodMap[mood] : '';
+};
+
+export const removeEntryElements = (date: string) => {
+  const existingContent = document.getElementById(`${date}-content`);
+  const existingButton = document.getElementById(`${date}-button`);
+  const existingDeleteButton = document.getElementById(`${date}-delete-button`);
+  const existingDescription = document.getElementById(`${date}-description`);
+
+  const dayTop = document.getElementById(`${date}-top`);
+  if (dayTop) dayTop.classList.remove('have-entry');
+  if (existingContent) existingContent.remove();
+  if (existingDescription) existingDescription.remove();
+  if (existingButton) existingButton.remove();
+  if (existingDeleteButton) existingDeleteButton.remove();
+};
