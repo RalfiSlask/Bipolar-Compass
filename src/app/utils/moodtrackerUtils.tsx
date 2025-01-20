7486ad23a4fd003155304838f65ad1d0675ca234
RalfiSlask <nilssonnarki@gmail.com>
2025-01-20 16:14:22 +0100

    utils: add util for getting react-icons based on the type of mood

---
+++ src/app/utils/moodtrackerUtils.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from 'react';
+import { FaAngry, FaBolt, FaRunning, FaSadTear, FaSmile } from 'react-icons/fa';
+import { GiDistressSignal, GiMeditation } from 'react-icons/gi';
+import { MdSentimentVerySatisfied } from 'react-icons/md';
+
+export const getMoodIcon = (moodName: string): ReactElement | null => {
+  const icons: Record<string, ReactElement> = {
+    'Ångest': <MdSentimentVerySatisfied className="w-6 h-6 text-yellow-500" />,
+    'Energi': <FaBolt className="w-6 h-6 text-red-500" />,
+    'Tränat': <FaRunning className="w-6 h-6 text-green-500" />,
+    'Socialt': <FaAngry className="w-6 h-6 text-orange-500" />,
+    'Sömn (h)': <GiMeditation className="w-6 h-6 text-blue-500" />,
+    'Depression': <FaSadTear className="w-6 h-6 text-blue-400" />,
+    'Stress': <GiDistressSignal className="w-6 h-6 text-purple-500" />,
+    'Lycka': <FaSmile className="w-6 h-6 text-yellow-400" />,
+  };
+
+  return icons[moodName] || null;
+};
