7a4cadbf558209986bde2ca32a836e88913befa1
RalfiSlask <nilssonnarki@gmail.com>
2025-01-22 09:48:20 +0100

    feat: add lightbox to be displayed at desktop when menu is open

---
+++ src/app/components/header/Lightbox.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { createPortal } from 'react-dom';
+
+interface LightboxProps {
+  onClose: () => void;
+}
+
+const Lightbox = ({ onClose }: LightboxProps) => {
+  if (typeof document === 'undefined') return null; // SSR check
+
+  return createPortal(
+    <div
+      className="fixed inset-0 bg-black/20"
+      onClick={onClose}
+      aria-hidden="true"
+      style={{ zIndex: 40 }}
+    />,
+    document.body
+  );
+};
+
+export default Lightbox; 
