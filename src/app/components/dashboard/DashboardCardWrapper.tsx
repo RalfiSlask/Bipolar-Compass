c0dae9ecd9745cea7f40b8e7ab0ad7ba798c4045
RalfiSlask <nilssonnarki@gmail.com>
2025-01-24 15:29:09 +0100

    feat: add dashboard cardwrapper to be used at my page in the dashboard

---
+++ src/app/components/dashboard/DashboardCardWrapper.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const DashboardCardWrapper = ({
+  children,
+  title,
+  icon: Icon,
+  className = '',
+}: {
+  children: React.ReactNode;
+  title: string;
+  icon: React.ElementType;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={`bg-white rounded-2xl shadow-lg p-6 mb-6 hover:shadow-xl transition-all duration-300 ${className}`}
+    >
+      <div className="flex items-center mb-4">
+        {Icon && <Icon className="mr-3 text-primary-accent text-2xl" />}
+        <h2 className="text-xl font-semibold text-primary-dark">{title}</h2>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default DashboardCardWrapper;
