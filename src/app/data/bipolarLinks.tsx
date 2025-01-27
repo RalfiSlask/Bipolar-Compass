8416106335f1c788ff10b90f03dbcc1a3c9388fc
RalfiSlask <nilssonnarki@gmail.com>
2025-01-27 10:05:50 +0100

    data: remove unneccesary dimensions, we use fill instead on images

--- src/app/data/bipolarLinks.tsx
+++ src/app/data/bipolarLinks.tsx
@@ -4,27 +4,23 @@ export const bipolarLinks = [
     href: '/bipolaritet/vad-ar-bipolaritet',
     description: 'Lär dig mer om vad bipolär sjukdom innebär',
     image: '/images/bipolar/bipolarity.webp',
-    imageDimensions: { width: 3900, height: 2600 },
   },
   {
     title: 'Symptom',
     href: '/bipolaritet/symptom',
     description: 'Vanliga tecken och symptom vid bipolär sjukdom',
     image: '/images/bipolar/symptoms.webp',
-    imageDimensions: { width: 3200, height: 2133 },
   },
   {
     title: 'Diagnoser',
     href: '/bipolaritet/diagnoser',
     description: 'Olika typer av bipolär sjukdom och diagnostisering',
     image: '/images/bipolar/diagnoses.webp',
-    imageDimensions: { width: 2096, height: 1397 },
   },
   {
     title: 'Vanliga frågor',
     href: '/bipolaritet/vanliga-fragor',
     description: 'Svar på ofta förekommande frågor',
     image: '/images/bipolar/faq.webp',
-    imageDimensions: { width: 1920, height: 1280 },
   },
 ];
