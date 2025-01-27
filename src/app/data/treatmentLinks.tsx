405fe7d8207e4fdff23e50cd432780182c43bb42
RalfiSlask <nilssonnarki@gmail.com>
2025-01-27 10:06:08 +0100

    data: we now use images instead of icons

--- src/app/data/treatmentLinks.tsx
+++ src/app/data/treatmentLinks.tsx
@@ -1,50 +1,30 @@
-import { BsChatSquareHeart, BsFileEarmarkText } from 'react-icons/bs';
-import { GiBrain } from 'react-icons/gi';
-import { RiMedicineBottleLine } from 'react-icons/ri';
-
 export const treatmentLinks = [
   {
     title: 'Medicin',
     href: '/behandling/medicinska-behandlingar',
     description:
       'Läs om olika typer av medicin som kan hjälpa vid bipolär sjukdom',
-    icon: (
-      <div className="p-3 rounded-full bg-green-100 shadow-lg ring-2 ring-green-500/20">
-        <RiMedicineBottleLine className="w-12 h-12 text-green-500" />
-      </div>
-    ),
+    image: '/images/treatments/medicine.webp',
   },
   {
     title: 'Självhjälp',
     href: '/behandling/sjalvhjalp',
     description:
       'Läs om olika självhjälpsmetoder som kan hjälpa vid bipolär sjukdom',
-    icon: (
-      <div className="p-3 rounded-full bg-rose-100 shadow-lg ring-2 ring-rose-500/20">
-        <GiBrain className="w-12 h-12 text-rose-500" />
-      </div>
-    ),
+    image: '/images/treatments/selfhelp.webp',
   },
   {
     title: 'Terapi',
     href: '/behandling/terapi',
     description:
       'Läs om olika typer av terapi som kan hjälpa vid bipolär sjukdom',
-    icon: (
-      <div className="p-3 rounded-full bg-red-100 shadow-lg ring-2 ring-red-500/20">
-        <BsChatSquareHeart className="w-12 h-12 text-red-500" />
-      </div>
-    ),
+    image: '/images/treatments/therapy.webp',
   },
   {
     title: 'Verktyg & Dokument',
     href: '/behandling/dokument',
     description:
       'Ladda ner krisplaner, självskattningsformulär och andra behandlingsverktyg',
-    icon: (
-      <div className="p-3 rounded-full bg-lime-100 shadow-lg ring-2 ring-lime-500/20">
-        <BsFileEarmarkText className="w-12 h-12 text-lime-500" />
-      </div>
-    ),
+    image: '/images/treatments/forms.webp',
   },
 ];
