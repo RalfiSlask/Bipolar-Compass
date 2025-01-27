88430473f44ae9eb64d51a27c17a8a76a5851bc7
RalfiSlask <nilssonnarki@gmail.com>
2025-01-27 10:18:13 +0100

    feat: improve bipolar  page with styling and additional text

--- src/app/bipolaritet/page.tsx
+++ src/app/bipolaritet/page.tsx
@@ -5,17 +5,21 @@ const BipolarPage = () => {
   return (
     <section className="min-h-screen ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-24">
-        <div className="mb-20 text-center">
-          <h2 className="text-5xl md:text-6xl font-bold text-primary-dark mb-8 animate-fade-in">
+        <div className="mb-20 text-left">
+          <h2 className="text-5xl md:text-6xl text-center font-bold text-primary-dark mb-8 animate-fade-in">
             Bipolär sjukdom
           </h2>
-          <p className="text-xl text-gray-700 max-w-3xl mx-auto  px-4">
-            Bipolär sjukdom är en psykisk sjukdom som kännetecknas av kraftiga
-            svängningar i stämningsläget. Personer med sjukdomen upplever
-            perioder av depression och mani eller hypomani. Här kan du läsa mer
-            om symtom, olika typer av diagnos och få svar på vanliga frågor om
-            sjukdomen.
-          </p>
+          <div className="flex flex-col gap-4 justify-center bg-primary-light  0 max-w-4xl mx-auto p-4 rounded-lg">
+            <p className="text-secondary-dark">
+              Bipolär sjukdom är en psykisk sjukdom som kännetecknas av kraftiga
+              svängningar i stämningsläget. Personer med sjukdomen upplever
+              perioder av depression och mani eller hypomani.
+            </p>
+            <p className="text-secondary-dark font-semibold">
+              Här kan du läsa mer om symtom, olika typer av diagnos och få svar
+              på vanliga frågor om sjukdomen.
+            </p>
+          </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto max-w-6xl">
