4e7ae95c52fbd8248c0cfbba418885806d20f0f6
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 19:07:54 +0100

    feat: update save medication route to schedule the reminders

--- src/app/api/settings/save/medications/route.ts
+++ src/app/api/settings/save/medications/route.ts
@@ -1,5 +1,7 @@
+import { IMedication } from '@/app/types/medication';
 import { IUser } from '@/app/types/user';
 import { getCollection } from '@/app/utils/databaseUtils';
+import { scheduleMedicationReminder } from '@/app/utils/qstashUtils';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const PUT = async (req: NextRequest): Promise<NextResponse> => {
@@ -13,6 +15,20 @@ export const PUT = async (req: NextRequest): Promise<NextResponse> => {
     }
 
     const updatedUser = { ...user, profile: { ...user.profile, medications } };
+
+    medications.forEach((medication: IMedication) => {
+      try {
+        if (user._id) {
+          console.log('Scheduling reminder for:', medication.name);
+          scheduleMedicationReminder(user._id.toString(), medication, email);
+        }
+      } catch (err) {
+        console.error(
+          `Failed to schedule reminder for ${medication.name}:`,
+          err
+        );
+      }
+    });
 
     await collection.updateOne({ email: email }, { $set: updatedUser });
     return NextResponse.json({ message: 'Medicines saved' });
