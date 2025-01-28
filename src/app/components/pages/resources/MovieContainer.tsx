9074a2294ed5bdf2b486219c1b1f40c6157ca176
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 08:16:54 +0100

    feat: add the imdb link, and add a nice hover effect on the movie container using group

--- src/app/components/pages/resources/MovieContainer.tsx
+++ src/app/components/pages/resources/MovieContainer.tsx
@@ -21,47 +21,58 @@ const MovieContainer = ({ movie }: IMovieContainerProps) => {
   const formattedRuntime = getFormattedTimeFromSeconds(runtimeInSeconds);
 
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-md">
-      <div className="aspect-[3/4] relative">
-        <Image
-          src={primaryImage ? primaryImage.url : 'saknar bild'}
-          alt={titleText ? titleText.text : 'en film'}
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div className="p-6 flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-2">
-            <h2 className="h-sm text-green-dark">{titleText?.text}</h2>
-            <p className="text-xl">({releaseDate?.year})</p>
-          </div>
-          <div className="flex items-center gap-4">
-            <p className="text-gray-600">{formattedRuntime}</p>
-            <div className="flex items-center gap-1">
-              <Image src={imdbLogo} width={40} height={40} alt="imdb loggan" />
-              <p className="font-medium">{ratingsSummary?.aggregateRating}</p>
+    <a
+      href={movie.imdbLink}
+      target="_blank"
+      className="block transform transition-transform group hover:shadow-lg duration-200"
+    >
+      <div className="bg-white rounded-lg overflow-hidden shadow-md">
+        <div className="aspect-[3/4] relative overflow-hidden">
+          <Image
+            src={primaryImage ? primaryImage.url : 'saknar bild'}
+            alt={titleText ? titleText.text : 'en film'}
+            fill
+            className="object-cover transition-transform duration-300 group-hover:scale-[1.03]"
+          />
+        </div>
+        <div className="p-6 flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <h2 className="h-sm text-green-dark">{titleText?.text}</h2>
+              <p className="text-xl">({releaseDate?.year})</p>
+            </div>
+            <div className="flex items-center gap-4">
+              <p className="text-gray-600">{formattedRuntime}</p>
+              <div className="flex items-center gap-1">
+                <Image
+                  src={imdbLogo}
+                  width={40}
+                  height={40}
+                  alt="imdb loggan"
+                />
+                <p className="font-medium">{ratingsSummary?.aggregateRating}</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-wrap gap-2">
-            {movieGenres.map((genre) => (
-              <div
-                className="bg-primary-light rounded-md px-3 py-1 text-sm"
-                key={genre.id}
-              >
-                {genre.text}
-              </div>
-            ))}
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-wrap gap-2">
+              {movieGenres.map((genre) => (
+                <div
+                  className="bg-primary-light rounded-md px-3 py-1 text-sm"
+                  key={genre.id}
+                >
+                  {genre.text}
+                </div>
+              ))}
+            </div>
+            <p className="text-gray-700 line-clamp-3">
+              {plot?.plotText?.plainText}
+            </p>
           </div>
-          <p className="text-gray-700 line-clamp-3">
-            {plot?.plotText?.plainText}
-          </p>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
