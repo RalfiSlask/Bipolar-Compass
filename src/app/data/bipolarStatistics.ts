5de25cdbe440a0906bee388ead88ec217fa4d59d
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 08:53:25 +0100

    data: move data relatad to showing bipolar statistics in here

---
+++ src/app/data/bipolarStatistics.ts
@@ -0,0 +1,26 @@
+export const Y_AXIS_LABELS = [
+  { id: 12, value: '15-19', label: '15-19', procent: 0.94 },
+  { id: 11, value: '20-24', label: '20-24', procent: 1.21 },
+  { id: 10, value: '25-29', label: '25-29', procent: 1.22 },
+  { id: 9, value: '30-34', label: '30-34', procent: 1.16 },
+  { id: 8, value: '35-39', label: '35-39', procent: 1.13 },
+  { id: 7, value: '40-44', label: '40-44', procent: 1.14 },
+  { id: 6, value: '45-49', label: '45-49', procent: 1.17 },
+  { id: 5, value: '50-54', label: '50-54', procent: 1.21 },
+  { id: 4, value: '55-59', label: '55-59', procent: 1.26 },
+  { id: 3, value: '60-64', label: '60-64', procent: 1.28 },
+  { id: 2, value: '65-69', label: '65-69', procent: 1.22 },
+  { id: 1, value: '70+', label: '70+', procent: 0.86 },
+  { id: 0, value: 'Alla åldrar', label: 'Alla åldrar', procent: 0.93 },
+];
+
+export const GENDER_DATA = [
+  { name: 'Kvinnor', value: 63 },
+  { name: 'Män', value: 37 },
+];
+
+export const BIPOLAR_TYPES = [
+  { name: 'Typ 1', value: 40 },
+  { name: 'Typ 2', value: 40 },
+  { name: 'Andra', value: 20 },
+];
