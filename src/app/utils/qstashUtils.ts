f93c87d28ba1b5dff5be52b57f9624e17586bdd2
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 19:08:32 +0100

    feat: add qstash helper function

---
+++ src/app/utils/qstashUtils.ts
@@ -0,0 +1,53 @@
+import { Client } from '@upstash/qstash';
+import { IMedication } from '../types/medication';
+
+const qstashClient = new Client({
+  token: process.env.QSTASH_TOKEN!,
+});
+
+export const scheduleMedicationReminder = async (
+  userId: string,
+  medication: IMedication,
+  email: string
+) => {
+  if (!medication.reminder.enabled) {
+    console.log(
+      `Skipping reminder for ${medication.name} (reminders disabled).`
+    );
+    return;
+  }
+
+  if (medication.times.length === 0) {
+    console.log(`Skipping reminder for ${medication.name} (no times set).`);
+    return;
+  }
+
+  medication.times.forEach(async (time: string) => {
+    console.log('Time:', time);
+    const [hours, minutes] = time.split(':').map(Number);
+    const medicationTime = new Date();
+    medicationTime.setUTCHours(hours, minutes, 0, 0);
+
+    if (medicationTime.getTime() <= Date.now()) {
+      medicationTime.setUTCDate(medicationTime.getUTCDate() + 1);
+    }
+
+    try {
+      await qstashClient.publishJSON({
+        url: `${process.env.NEXTAUTH_URL}/api/send-email`,
+        body: {
+          email,
+          medication,
+          userId,
+          time,
+        },
+        notBefore: Math.floor(medicationTime.getTime() / 1000),
+      });
+    } catch (error) {
+      console.error(
+        `Failed to schedule reminder for ${medication.name}:`,
+        error
+      );
+    }
+  });
+};
