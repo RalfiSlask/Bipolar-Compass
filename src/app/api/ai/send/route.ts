06182da2406b04003d10e89580fb4a8bcfcb3a68
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 10:39:37 +0100

    bug: fix type errors

--- src/app/api/ai/send/route.ts
+++ src/app/api/ai/send/route.ts
@@ -1,41 +1,44 @@
 import { AiConversation, ConversationLog } from '@/app/models/Ai';
-import { IConversationLog } from '@/app/types/ai';
+import { IAiConversation, IConversationLog } from '@/app/types/ai';
 import { getCollection } from '@/app/utils/databaseUtils';
+import { OptionalId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import OpenAI from 'openai';
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
 export const POST = async (req: NextRequest) => {
   try {
     const { message, userId } = await req.json();
     const AiCollection = await getCollection('thesis', 'conversations');
 
-    let conversation = await AiCollection.findOne({
+    let conversation = await AiCollection.findOne<IAiConversation>({
       user_id: userId.toString(),
     });
 
     if (!conversation) {
-      console.log('Creating new conversation for userId:', userId);
       const newConversation = new AiConversation([]);
       newConversation.user_id = userId.toString();
-      const result = await AiCollection.insertOne(newConversation);
+      const result = await AiCollection.insertOne(
+        newConversation.toDBObject() as unknown as OptionalId<Document>
+      );
       conversation = {
         ...newConversation,
         _id: result.insertedId,
       };
     }
     const botCollection = await getCollection('thesis', 'ai_assistant');
-    const botData = await botCollection.findOne({
+    const botData = await botCollection.findOne<IConversationLog>({
       name: 'Mental Health Assistant',
     });
     const systemPrompt = botData?.role || 'Du är en expert på bipolaritet...';
 
     const messages = [
       { role: 'system', content: systemPrompt },
-      ...conversation.conversation_log.map((log: IConversationLog) => ({
+      ...conversation?.conversation_log.map((log: IConversationLog) => ({
         role: log.role,
         content: log.content,
       })),
@@ -44,7 +47,7 @@ export const POST = async (req: NextRequest) => {
 
     const chatResponse = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
-      messages: messages,
+      messages: messages as ChatCompletionMessageParam[],
     });
 
     const assistantMessage = chatResponse.choices[0].message.content;
@@ -59,7 +62,6 @@ export const POST = async (req: NextRequest) => {
       new ConversationLog('user', message),
       new ConversationLog('assistant', assistantMessage),
     ];
-    console.log('Updated logs:', updatedLogs);
 
     await AiCollection.updateOne(
       { user_id: userId.toString() },
