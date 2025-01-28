671c55bff6776e03f39bc33a38570f8202bcb90c
RalfiSlask <nilssonnarki@gmail.com>
2025-01-28 08:52:53 +0100

    data: move objects related to the password strength indicator into its own file

---
+++ src/app/data/passwordIndicator.ts
@@ -0,0 +1,26 @@
+export const PASSWORD_STRENGTH_MESSAGES = {
+  0: 'Mycket svagt',
+  1: 'Svagt',
+  2: 'Medel',
+  3: 'Bra',
+  4: 'Starkt',
+  5: 'Godkänt',
+};
+
+export const PASSWORD_COLORS = {
+  0: 'bg-red-500',
+  1: 'bg-orange-500',
+  2: 'bg-yellow-500',
+  3: 'bg-lime-500',
+  4: 'bg-green-700',
+  5: 'bg-primary-medium',
+};
+
+export const PASSWORD_TEXT_COLORS = {
+  0: 'text-red-700',
+  1: 'text-orange-700',
+  2: 'text-yellow-700',
+  3: 'text-lime-700',
+  4: 'text-green-700',
+  5: 'text-primary-medium',
+};
