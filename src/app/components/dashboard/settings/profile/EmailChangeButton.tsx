ba98ed2177b1ee7b0097abb8d890d5fcf140d9d4
RalfiSlask <nilssonnarki@gmail.com>
2025-02-01 14:30:13 +0100

    feat: move out email change button and change some colors to improve UX

---
+++ src/app/components/dashboard/settings/profile/EmailChangeButton.tsx
@@ -0,0 +1,26 @@
+interface IEmailChangeButtonProps {
+  isEmailChangable: boolean;
+  changeEmailChangable: () => void;
+}
+
+const EmailChangeButton = ({
+  isEmailChangable,
+  changeEmailChangable,
+}: IEmailChangeButtonProps) => {
+  const emailChangableColors = isEmailChangable
+    ? 'bg-primary-light hover:bg-primary-medium text-gray-900 hover:text-white'
+    : 'bg-white hover:bg-gray-50 text-gray-700';
+
+  return (
+    <button
+      type="button"
+      className={`whitespace-nowrap h-[44px] px-4 text-sm font-medium rounded-md border shadow-sm min-w-[130px]
+      ${emailChangableColors}`}
+      onClick={changeEmailChangable}
+    >
+      {isEmailChangable ? 'Spara' : 'Ändra e-post'}
+    </button>
+  );
+};
+
+export default EmailChangeButton;
