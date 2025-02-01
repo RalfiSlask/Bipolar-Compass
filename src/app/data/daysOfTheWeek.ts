7c5963f5551470de9ebf0fa8fc636ddb7f3bd374
RalfiSlask <nilssonnarki@gmail.com>
2025-02-01 14:29:32 +0100

    data: move days of the week data from my page to its own data file

---
+++ src/app/data/daysOfTheWeek.ts
@@ -0,0 +1,19 @@
+export const SWEDISH_DAYS_OF_THE_WEEK = [
+  'måndag',
+  'tisdag',
+  'onsdag',
+  'torsdag',
+  'fredag',
+  'lördag',
+  'söndag',
+];
+
+export const ENGLISH_DAYS_OF_THE_WEEK = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+];
