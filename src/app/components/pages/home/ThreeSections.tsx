00c0a006fc3ae311fe8150b25e7876cd5b3022ad
RalfiSlask <nilssonnarki@gmail.com>
2025-02-02 07:52:18 +0100

    bug: fix so that the apple image has the right name

--- src/app/components/pages/home/ThreeSections.tsx
+++ src/app/components/pages/home/ThreeSections.tsx
@@ -1,7 +1,6 @@
 import { threeSections } from '@/app/data/home';
 import Image from 'next/image';
 import Link from 'next/link';
-import AppleImage from '../../../../../public/images/apple.webp';
 
 interface SectionProps {
   title: string;
@@ -39,7 +38,7 @@ const ThreeSections = () => {
       <div className="w-full flex flex-col md:flex-row items-center justify-end max-w-[1440px] md:min-h-[600px] rounded-3xl md:-my-32">
         <div className="relative w-full md:w-1/4 h-[300px] md:h-[600px] flex items-center justify-end overflow-hidden">
           <Image
-            src={AppleImage}
+            src="/images/home/apple.webp"
             alt="Bipolar logo"
             fill
             sizes="(max-width: 768px) 100vw, 25vw"
