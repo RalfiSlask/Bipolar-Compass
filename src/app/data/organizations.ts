4e3425edaae5d32d781db6faa115eb510fb7cab8
RalfiSlask <nilssonnarki@gmail.com>
2025-05-22 11:37:01 +0200

    data: add data for organizations

---
+++ src/app/data/organizations.ts
@@ -0,0 +1,75 @@
+
+export const ORGANIZATIONS = [
+  {
+    id: 1,
+    href: 'https://balansriks.se/balans-foreningar/',
+    src: '/images/organizations/balans.png',
+    alt: 'bild på organisationen balans riks',
+    title: 'Balans Riks',
+    description:
+      'Balans är en rikstäckande ideell patient- och anhörig organisation för personer med bipolär sjukdom, depression och andra psykiska ohälsotillstånd. De erbjuder stöd, information och gemenskap för både drabbade och anhöriga.',
+  },
+  {
+    id: 2,
+    href: 'https://anhorigasriksforbund.se/',
+    src: '/images/organizations/anhoriga-riksforbund.png',
+    alt: 'bild på organisationen anhörigas riksförbund',
+    title: 'Anhörigas Riksförbund',
+    description:
+      'Anhörigas Riksförbund är en partipolitiskt och religiöst obunden organisation som stödjer anhöriga och anhörigvårdare oavsett ålder, kön eller diagnos. De arbetar för att förbättra anhörigas situation genom opinionsbildning och erfarenhetsutbyte.',
+  },
+  {
+    id: 3,
+    href: 'https://mind.se/stod-kunskap/prata-eller-chatta-med-volontar/foraldralinjen/',
+    src: '/images/organizations/mind.png',
+    alt: 'bild på organisationen mind',
+    title: 'Mind',
+    description:
+      'Mind driver Föräldralinjen, en stödlinje för föräldrar och andra vuxna som är oroliga för barn eller ungdomar. De erbjuder anonymt och kostnadsfritt stöd via telefon och chatt, och arbetar för psykisk hälsa och att förebygga psykisk ohälsa och självmord.',
+  },
+  {
+    id: 4,
+    href: 'https://nsph.se/',
+    src: '/images/organizations/nsph.png',
+    alt: 'nationell samverkan för psykisk hälsa bild',
+    title: 'Nationell Samverkan för Psykisk Hälsa',
+    description:
+      'NSPH är ett nätverk av patient-, brukar- och anhörigorganisationer inom psykisk hälsa. De arbetar för att människor med psykisk ohälsa ska ges möjlighet till återhämtning och delaktighet i samhället på jämlika villkor.',
+  },
+  {
+    id: 5,
+    href: 'https://spes.se/',
+    src: '/images/organizations/spes.png',
+    alt: 'SPES - Riksförbundet för SuicidPrevention och Efterlevandes Stöd bild',
+    title: 'SPES',
+    description:
+      'SPES (Riksförbundet för SuicidPrevention och Efterlevandes Stöd) är en rikstäckande organisation som stödjer efterlevande vid självmord och arbetar för att förebygga suicid. De erbjuder samtalsstöd, träffar och olika aktiviteter för efterlevande.',
+  },
+  {
+    id: 6,
+    href: 'https://www.bris.se/',
+    src: '/images/organizations/bris.png',
+    alt: 'barnens rätt i samhället bild',
+    title: 'Barnens Rätt i Samhället',
+    description:
+      'BRIS är en barnrättsorganisation som stöttar barn och unga, mobiliserar samhället och påverkar beslutsfattare för ett bättre samhälle för barn. De erbjuder professionellt samtalsstöd via telefon, chatt och mail för barn upp till 18 år.',
+  },
+  {
+    id: 7,
+    href: 'https://suicidezero.se/',
+    src: '/images/organizations/suicide-zero.png',
+    alt: 'suicide zero bild',
+    title: 'Suicide Zero',
+    description:
+      'Suicide Zero är en ideell organisation som arbetar för att radikalt minska självmorden i Sverige. De sprider kunskap, bedriver opinionsbildning och stödjer forskning för att förebygga psykisk ohälsa och självmord.',
+  },
+  {
+    id: 8,
+    href: 'https://anhoriga.se/',
+    src: '/images/organizations/nka.png',
+    alt: 'bild på organisationen Nationellt Kompetenscentrum anhöriga',
+    title: 'Nationellt Kompetenscentrum Anhöriga',
+    description:
+      'NKA är ett expertcentrum som arbetar för att utveckla framtidens anhörigstöd. De samlar och sprider kunskap om anhörigfrågor, utvecklar metoder och verktyg för anhörigstöd, samt erbjuder utbildningar och konferenser.',
+  },
+];
