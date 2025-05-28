64625dd98b130b2b91d379b147e23801f8a7bc92
RalfiSlask <nilssonnarki@gmail.com>
2025-05-28 12:39:31 +0200

    data: add type & name to relatives

--- src/app/models/Relative.ts
+++ src/app/models/Relative.ts
@@ -3,6 +3,8 @@ export class Relative implements IRelative {
   constructor(
     public email: string = '',
     public email_enabled: boolean = false,
-    public email_frequency: 'weekly' | 'biweekly' | 'monthly' = 'weekly'
+    public email_frequency: 'weekly' | 'biweekly' | 'monthly' = 'weekly',
+    public type: 'parent' | 'child' | 'doctor' | 'friend' | 'caregiver' | 'other' = 'other',
+    public name: string = '',
   ) {}
 }
