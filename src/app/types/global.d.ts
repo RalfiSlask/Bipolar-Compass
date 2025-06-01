2e4a47cac222103a953d2d4d85d63ece964ac895
RalfiSlask <nilssonnarki@gmail.com>
2025-06-01 07:56:09 +0200

    types: define types for Window for Speech Recognition feature

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,6 +1,11 @@
 // global.d.ts
 declare global {
   const _mongoClientPromise: Promise<MongoClient>;
+  interface Window {
+    webkitAudioContext: typeof AudioContext;
+    webkitSpeechRecognition: typeof SpeechRecognition;
+    SpeechRecognition: typeof SpeechRecognition;
+  }
 }
 
 export {};
