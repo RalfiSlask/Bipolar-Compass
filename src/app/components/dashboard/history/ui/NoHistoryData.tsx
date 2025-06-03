8860017b05d350c151af8d2d4c38d509ce7cc5fa
RalfiSlask <nilssonnarki@gmail.com>
2025-06-03 12:36:11 +0200

    feat: a simple UI history component

---
+++ src/app/components/dashboard/history/ui/NoHistoryData.tsx
@@ -0,0 +1,12 @@
+const NoHistoryData = () => {
+  return (
+    <div className="text-center text-dark p-6 sm:p-8 bg-white rounded-xl shadow-lg">
+      <p className="mb-4 text-base sm:text-lg">Ingen data tillgänglig</p>
+      <p className="text-xs sm:text-sm text-gray-500">
+        Börja registrera ditt mående i Moodtracker för att se din historik här
+      </p>
+    </div>
+  );
+};
+
+export default NoHistoryData;
