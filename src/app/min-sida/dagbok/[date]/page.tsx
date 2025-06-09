fee1e18241e6f826f5d36373aa49d6a2ba3706e2
RalfiSlask <nilssonnarki@gmail.com>
2025-06-09 09:47:17 +0200

    chore: replace code with the new delete modal component

--- src/app/min-sida/dagbok/[date]/page.tsx
+++ src/app/min-sida/dagbok/[date]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import DeleteModal from '@/app/components/dashboard/diary/DeleteModal';
 import CustomSelect from '@/app/components/shared/CustomSelectDropdown';
 import Spinner from '@/app/components/shared/Spinner';
 import { diaryMoods } from '@/app/data/diary';
@@ -276,36 +277,10 @@ const DiaryNote = () => {
         </div>
       </div>
       {isDeleteModalOpen && (
-        <>
-          <div className="fixed inset-0 bg-black bg-opacity-50 z-20 w-full h-full"></div>
-          <div className="flex flex-col text-lg justify-center items-center gap-6 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-30 bg-white p-8 rounded-lg shadow-lg">
-            <div className="flex flex-col text-center">
-              <h2 className="h-sm font-semibold">
-                Är du säker på att du vill ta bort detta dagboksinlägg?
-              </h2>
-              <p className="text-gray-600 text-sm mt-2">
-                Detta är en permanent åtgärd och kan inte ångras.
-              </p>
-            </div>
-            <div className="flex gap-4">
-              <button
-                onClick={() => {
-                  deleteDiaryEntry();
-                  closeDeleteModal();
-                }}
-                className="primary-button py-1"
-              >
-                Ja
-              </button>
-              <button
-                onClick={closeDeleteModal}
-                className="secondary-button py-1"
-              >
-                Nej
-              </button>
-            </div>
-          </div>
-        </>
+        <DeleteModal
+          deleteDiaryEntry={deleteDiaryEntry}
+          closeDeleteModal={closeDeleteModal}
+        />
       )}
     </div>
   );
