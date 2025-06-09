63116e7ed244c8a359c9509dbb628ce34bf49dce
RalfiSlask <nilssonnarki@gmail.com>
2025-06-09 09:19:34 +0200

    chore: replace code with new components

--- src/app/akut/page.tsx
+++ src/app/akut/page.tsx
@@ -1,24 +1,23 @@
 'use client';
 
-import Image from 'next/image';
 import { useState } from 'react';
 import { BsLightbulb } from 'react-icons/bs';
 import { FaPhoneVolume } from 'react-icons/fa6';
 import { IoMdHeart } from 'react-icons/io';
-import { MdLocalHospital, MdWarning } from 'react-icons/md';
-import googleMapsIcon from '../assets/icons/google-maps.svg';
+import { MdWarning } from 'react-icons/md';
 import EmergencyService from '../components/pages/acute/EmergencyService';
+import Hospital from '../components/pages/acute/Hospital';
 import { emergencyServices } from '../data/emegencyServices';
 
 const UrgentHelpPage = () => {
   const [hospitalLink, setHospitalLink] = useState('');
   const [error, setError] = useState('');
 
-  /* 
-    This function is used to find the nearest hospital based on the user's location.
-    It uses the navigator.geolocation API to get the user's location.
-    If the user's location is found, it sets the hospital link to the Google Maps link for the user's location.
-  */
+  /**
+   * This function is used to find the nearest hospital based on the user's location.
+   * It uses the navigator.geolocation API to get the user's location.
+   * If the user's location is found, it sets the hospital link to the Google Maps link for the user's location.
+   */
   const findNearestHospital = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -64,7 +63,6 @@ const UrgentHelpPage = () => {
           </p>
         </div>
       </div>
-
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-3">
           <FaPhoneVolume className="text-primary-medium text-2xl" />
@@ -76,56 +74,11 @@ const UrgentHelpPage = () => {
           })}
         </ul>
       </div>
-
-      <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-3">
-          <MdLocalHospital className="text-primary-medium text-2xl" />
-          <h3 className="h-sm text-primary-dark">Närliggande sjukhus</h3>
-        </div>
-        <div className="flex flex-col gap-2 bg-primary-light rounded-lg p-4 shadow-md">
-          {hospitalLink ? (
-            <p className="text-primary-dark mb-2">Hitta sjukhus via kartan: </p>
-          ) : (
-            <p className="text-lg">
-              Klicka på knappen nedan för att hitta sjukhus nära dig:
-            </p>
-          )}
-
-          {!hospitalLink && !error && (
-            <button
-              className="primary-button mt-2 w-[250px] hover:bg-primary-medium transition-colors"
-              onClick={findNearestHospital}
-            >
-              Hitta närmaste sjukhus
-            </button>
-          )}
-          {hospitalLink && (
-            <p className="text-lg">
-              <a
-                href={hospitalLink}
-                className="flex gap-2 items-center hover:text-primary-medium transition-colors"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src={googleMapsIcon}
-                  alt="google maps ikon"
-                  width={24}
-                  height={24}
-                />
-                Öppna i Google Maps
-              </a>
-            </p>
-          )}
-        </div>
-
-        {error && (
-          <p className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
-            {error}
-          </p>
-        )}
-      </div>
-
+      <Hospital
+        hospitalLink={hospitalLink}
+        error={error}
+        findNearestHospital={findNearestHospital}
+      />
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-3">
           <IoMdHeart className="text-primary-medium text-2xl" />
