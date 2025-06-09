67df6f394ddbafbdefd4848767c3ee36ceda3284
RalfiSlask <nilssonnarki@gmail.com>
2025-06-09 09:18:17 +0200

    feat: add data for the guide lists

---
+++ src/app/data/tools/tools.tsx
@@ -0,0 +1,11 @@
+export const LEFT_TOOL_GUIDE_LIST = [
+  'Igenkänning av tidiga varningstecken',
+  'Strategier för att hantera både maniska och depressiva episoder',
+  'Tips för bättre sömnrutiner och stresshantering',
+];
+
+export const RIGHT_TOOL_GUIDE_LIST = [
+  'Råd om medicinering och behandling',
+  'Information för anhöriga',
+  'Kontaktinformation till vårdresurser',
+];
