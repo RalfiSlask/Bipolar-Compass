8548e44d32867ec82ea254ce99d0e42d1730d882
RalfiSlask <nilssonnarki@gmail.com>
2025-06-16 18:03:43 +0200

    chore: fix import

--- src/app/hjalp/juridik-och-raattigheter/page.tsx
+++ src/app/hjalp/juridik-och-raattigheter/page.tsx
@@ -3,11 +3,11 @@ import AgreementRights from '@/app/components/pages/help/law-and-rights/Agreemen
 import BenefitCard from '@/app/components/pages/help/law-and-rights/BenefitCard';
 import LPT from '@/app/components/pages/help/law-and-rights/LPT';
 import LawFirms from '@/app/components/pages/help/law-and-rights/LawFirms';
+import OfficialSourcesList from '@/app/components/pages/help/law-and-rights/OfficialSourcesList';
 import PatientRights from '@/app/components/pages/help/law-and-rights/PatientRights';
 import RightsFlowChart from '@/app/components/pages/help/law-and-rights/RightsFlowChart';
 import WorkRights from '@/app/components/pages/help/law-and-rights/WorkRights';
 import EncouragmentContainer from '@/app/components/shared/EncouragmentContainer';
-import OfficialSourcesList from '@/app/components/shared/OfficialSourcesList';
 import PageIntroContainer from '@/app/components/shared/PageIntroContainer';
 import SectionTitle from '@/app/components/shared/headings/SectionTitle';
 import {
