d54f43eb040f688d2e04fef144a4a93e2df9748e
RalfiSlask <nilssonnarki@gmail.com>
2025-06-16 17:31:47 +0200

    data: data array for the different categories we will be showing books in

---
+++ src/app/data/books.ts
@@ -0,0 +1,37 @@
+export const BOOK_CATEGORIES = [
+  {
+    id: 1,
+    name: 'Psychology',
+    label: 'Psykologi',
+  },
+  {
+    id: 2,
+    name: 'Medical',
+    label: 'Medicin',
+  },
+  {
+    id: 3,
+    name: 'Self-Help',
+    label: 'Självhjälp',
+  },
+  {
+    id: 4,
+    name: 'Health & Fitness',
+    label: 'Hälsa & Fitness',
+  },
+  {
+    id: 5,
+    name: 'Family & Relationships',
+    label: 'Familj & Relationer',
+  },
+  {
+    id: 6,
+    name: 'Education',
+    label: 'Utbildning',
+  },
+  {
+    id: 7,
+    name: 'Biography & Autobiography',
+    label: 'Biografi',
+  },
+];
