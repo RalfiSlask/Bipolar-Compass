34a5ba24e6dc03fc70a3dfdbff10120341dc575f
RalfiSlask <nilssonnarki@gmail.com>
2025-06-16 17:06:52 +0200

    feat: reusable component for links with arrows with props for color & size

---
+++ src/app/components/shared/links/LinkWithArrow.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { HiArrowRight } from 'react-icons/hi';
+
+interface ILinkWithArrowProps {
+  link: string;
+  text: string;
+  color?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'sm' | 'lg' | 'xl';
+}
+
+const LinkWithArrow = ({ link, text, color, size }: ILinkWithArrowProps) => {
+  const colorClasses = {
+    primary: 'text-primary-dark hover:text-primary-accent',
+    secondary: 'text-secondary-dark',
+    tertiary: 'text-tertiary-dark hover:text-tertiary-accent',
+  };
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`text-${
+        size ?? 'base'
+      } font-semibold inline-flex gap-1 text- items-center group ${
+        colorClasses[color ?? 'primary']
+      }`}
+    >
+      {text}
+      <HiArrowRight
+        className={`group-hover:translate-x-0.5 transition-transform text-${
+          size ?? 'base'
+        }`}
+      />
+    </Link>
+  );
+};
+
+export default LinkWithArrow;
