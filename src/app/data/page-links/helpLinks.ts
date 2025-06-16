e7ee02cec71fdd1a6ce68899c9fe6a83a0b93406
RalfiSlask <nilssonnarki@gmail.com>
2025-06-16 17:04:45 +0200

    data: add help links array populated with information about the law page

---
+++ src/app/data/page-links/helpLinks.ts
@@ -0,0 +1,8 @@
+export const HELP_LINKS = [
+  {
+    title: 'Juridik och rättigheter',
+    href: '/hjalp/juridik-och-raattigheter',
+    description: 'Lär dig mer om vad juridik och rättigheter innebär',
+    image: '/images/help/law/law.webp',
+  },
+];
