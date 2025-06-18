4b4a7a041f28941d4f562adde2bd06e285061fae
RalfiSlask <nilssonnarki@gmail.com>
2025-06-18 07:37:55 +0200

    feat: use the reusable component instead

--- src/app/behandling/page.tsx
+++ src/app/behandling/page.tsx
@@ -1,39 +1,8 @@
-import MainPageImageGrid from '../components/shared/MainPageImageGrid';
-import { TREATMENT_LINKS } from '../data/treatment/treatmentLinks';
+import MainLinkPageLayout from '../components/shared/PageLayout';
+import { PAGE_LAYOUT_DATA } from '../data/pageLayoutLinks';
 
 const TreatmentPage = () => {
-  return (
-    <section className="min-h-screen ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 sm:pt-16 pb-16 sm:pb-24">
-        <div className="mb-8 sm:mb-20 text-left">
-          <h2 className="text-5xl md:text-6xl text-center font-bold text-primary-dark mb-8 animate-fade-in">
-            Behandling
-          </h2>
-          <div className="flex flex-col gap-4 justify-center bg-primary-light  0 max-w-4xl mx-auto p-4 rounded-lg">
-            <p className="text-secondary-dark">
-              Bipolär sjukdom är en psykisk sjukdom som kännetecknas av kraftiga
-              svängningar i stämningsläget. Personer med sjukdomen upplever
-              perioder av djup depression där energinivån och motivationen är
-              låg, och perioder av mani eller hypomani som kännetecknas av
-              förhöjd energi, rastlöshet och ibland impulsivitet. Dessa
-              svängningar kan påverka både det sociala livet och arbetsförmågan,
-              och utan rätt behandling kan de bli svåra att hantera.
-            </p>
-            <p className="text-secondary-dark font-semibold">
-              Oavsett om du är patient, anhörig eller vårdgivare finns det
-              resurser som kan ge dig värdefull kunskap och vägledning.
-            </p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto max-w-6xl">
-          {TREATMENT_LINKS.map((item) => (
-            <MainPageImageGrid key={item.href} item={item} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+  return <MainLinkPageLayout pageInfo={PAGE_LAYOUT_DATA.treatment} />;
 };
 
 export default TreatmentPage;
--- src/app/bipolaritet/page.tsx
+++ src/app/bipolaritet/page.tsx
@@ -1,35 +1,8 @@
-import MainPageImageGrid from '../components/shared/MainPageImageGrid';
-import { bipolarLinks } from '../data/bipolar/bipolarLinks';
+import MainLinkPageLayout from '../components/shared/PageLayout';
+import { PAGE_LAYOUT_DATA } from '../data/pageLayoutLinks';
 
 const BipolarPage = () => {
-  return (
-    <section className="min-h-screen ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 sm:pt-16 pb-16 sm:pb-24">
-        <div className="mb-8 sm:mb-20 text-left">
-          <h2 className="text-5xl md:text-6xl text-center font-bold text-primary-dark mb-8 animate-fade-in">
-            Bipolär sjukdom
-          </h2>
-          <div className="flex flex-col gap-4 justify-center bg-primary-light  0 max-w-4xl mx-auto p-4 rounded-lg">
-            <p className="text-secondary-dark">
-              Bipolär sjukdom är en psykisk sjukdom som kännetecknas av kraftiga
-              svängningar i stämningsläget. Personer med sjukdomen upplever
-              perioder av depression och mani eller hypomani.
-            </p>
-            <p className="text-secondary-dark font-semibold">
-              Här kan du läsa mer om symtom, olika typer av diagnos och få svar
-              på vanliga frågor om sjukdomen.
-            </p>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto max-w-6xl">
-          {bipolarLinks.map((item) => (
-            <MainPageImageGrid key={item.href} item={item} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+  return <MainLinkPageLayout pageInfo={PAGE_LAYOUT_DATA.bipolar} />;
 };
 
 export default BipolarPage;
