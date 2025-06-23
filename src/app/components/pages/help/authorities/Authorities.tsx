4d865db93cadb7110eb1f76f941229b8214b0579
RalfiSlask <nilssonnarki@gmail.com>
2025-06-23 16:51:47 +0200

    feat: component for all the authoritites

---
+++ src/app/components/pages/help/authorities/Authorities.tsx
@@ -0,0 +1,24 @@
+import useAuthoritiesContext from '@/app/hooks/useAuthoritiesContext';
+import { ColorType } from '@/app/types/colorTypes';
+import AuthorityCard from './AuthorityCard';
+import NoSearchResults from './NoSearchResults';
+
+const Authorities = ({ type = 'primary' }: { type?: ColorType }) => {
+  const { filteredAuthorities } = useAuthoritiesContext();
+
+  return (
+    <>
+      {filteredAuthorities.length > 0 ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {filteredAuthorities.map((authority, index) => (
+            <AuthorityCard key={index} authority={authority} type={type} />
+          ))}
+        </div>
+      ) : (
+        <NoSearchResults type={type} />
+      )}
+    </>
+  );
+};
+
+export default Authorities;
