6c5daa2af88c53830810f68cc64eb58a29796c62
RalfiSlask <nilssonnarki@gmail.com>
2025-06-23 16:56:03 +0200

    feat: add intro section for the authoritites grid

--- src/app/components/pages/help/authorities/AuthoritiesGrid.tsx
+++ src/app/components/pages/help/authorities/AuthoritiesGrid.tsx
@@ -1,36 +1,15 @@
-import {
-  AuthoritiesSearchProvider,
-  useAuthoritiesSearchContext,
-} from '@/app/context/AuthoritiesSearchContext';
+import { AuthoritiesSearchProvider } from '@/app/context/AuthoritiesSearchContext';
 import { ColorType } from '@/app/types/colorTypes';
+import Authorities from './Authorities';
+import AuthoritiesFilter from './AuthoritiesFilter';
+import AuthoritiesResultsCount from './AuthoritiesResultsCount';
 import AuthoritiesSearch from './AuthoritiesSearch';
-import AuthorityCard from './AuthorityCard';
-import NoSearchResults from './NoSearchResults';
 
 interface IAuthoritiesGridProps {
   title?: string;
   description?: string;
   type?: ColorType;
 }
-
-const AuthoritiesGridContent = ({ type = 'primary' }: { type?: ColorType }) => {
-  const { filteredAuthorities } = useAuthoritiesSearchContext();
-
-  return (
-    <>
-      <AuthoritiesSearch type={type} />
-      {filteredAuthorities.length > 0 ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-          {filteredAuthorities.map((authority, index) => (
-            <AuthorityCard key={index} authority={authority} type={type} />
-          ))}
-        </div>
-      ) : (
-        <NoSearchResults type={type} />
-      )}
-    </>
-  );
-};
 
 const AuthoritiesGrid = ({
   title,
@@ -40,7 +19,7 @@ const AuthoritiesGrid = ({
   return (
     <AuthoritiesSearchProvider>
       <div
-        className={`bg-${type}-light rounded-xl p-6 shadow-lg border-2 border-${type}-light/60`}
+        className={`bg-${type}-light rounded-xl p-4 sm:p-6 shadow-lg border-2 border-${type}-light/50`}
       >
         {(title || description) && (
           <div className="mb-6">
@@ -55,7 +34,10 @@ const AuthoritiesGrid = ({
           </div>
         )}
 
-        <AuthoritiesGridContent type={type} />
+        <AuthoritiesSearch type={type} />
+        <AuthoritiesFilter type={type} />
+        <AuthoritiesResultsCount type={type} />
+        <Authorities type={type} />
       </div>
     </AuthoritiesSearchProvider>
   );
