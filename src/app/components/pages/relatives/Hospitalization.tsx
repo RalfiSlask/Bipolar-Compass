95be72eccf2319c15f08a6e5862dd02f9293a84d
RalfiSlask <nilssonnarki@gmail.com>
2025-06-25 12:26:51 +0200

    styles: center the image on the y-axis

--- src/app/components/pages/relatives/Hospitalization.tsx
+++ src/app/components/pages/relatives/Hospitalization.tsx
@@ -45,7 +45,7 @@ const Hospitalization = () => {
         </div>
       </div>
 
-      <div className="flex-1 h-full flex flex-col gap-6 items-center">
+      <div className="flex-1 h-full flex flex-col gap-6 items-center justify-center">
         <div className="w-full">
           <Image
             src="/images/relatives/hospital.webp"
