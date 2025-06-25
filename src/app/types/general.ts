8818ac683b2e583749f6d14b71d84ac45ca2cc71
RalfiSlask <nilssonnarki@gmail.com>
2025-06-25 12:22:00 +0200

    types: we add a general interface for the section with a rounded image and a list

---
+++ src/app/types/general.ts
@@ -0,0 +1,17 @@
+import { ColorType } from './colorTypes';
+
+export interface IListItem {
+  text: string;
+}
+
+export interface IRoundedImageWithListData {
+  title?: string;
+  desc: string;
+  subdesc?: string;
+  listItems: IListItem[];
+  image: string;
+  imageAlt: string;
+  type?: ColorType;
+  alignment?: 'left' | 'right';
+  listIcon?: React.ComponentType<{ className?: string }>;
+}
