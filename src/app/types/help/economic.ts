7739629afeaf27f5c037935765fb872d13a177ec
RalfiSlask <nilssonnarki@gmail.com>
2025-06-25 12:22:34 +0200

    types: add relevant types for the economic section

---
+++ src/app/types/help/economic.ts
@@ -0,0 +1,32 @@
+import { ColorType } from '../colorTypes';
+
+export interface IRowItem {
+  title: string;
+  id: number;
+}
+
+export interface ITableRow {
+  id: number;
+  cells: IRowItem[];
+  bodyColor?: string;
+  borderColor?: string;
+}
+
+export interface IRecommendationListItem {
+  id: number;
+  title: string;
+}
+
+export interface IRecommendationContainer {
+  id: number;
+  title: string;
+  description: string;
+  listItems: IRecommendationListItem[];
+  type: ColorType;
+}
+
+export interface IImportantReminder {
+  title: string;
+  description: string;
+  type: ColorType;
+}
