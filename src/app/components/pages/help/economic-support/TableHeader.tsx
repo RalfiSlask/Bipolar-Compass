5c44bb7e0105898285fe2d6c2e2aac32ddd95030
RalfiSlask <nilssonnarki@gmail.com>
2025-06-25 12:26:24 +0200

    feat: add components to be used in table headers and table bodies

---
+++ src/app/components/pages/help/economic-support/TableHeader.tsx
@@ -0,0 +1,30 @@
+import { IRowItem } from '@/app/types/help/economic';
+
+interface ITableHeaderProps {
+  rowItems: IRowItem[];
+  bgColor?: string;
+  borderColor?: string;
+}
+
+const TableHeader = ({
+  rowItems,
+  bgColor = 'primary-light',
+  borderColor = 'gray-300',
+}: ITableHeaderProps) => {
+  return (
+    <thead>
+      <tr className={`bg-${bgColor}`}>
+        {rowItems.map((item) => (
+          <th
+            key={item.id}
+            className={`border border-${borderColor} p-3 text-left font-semibold`}
+          >
+            {item.title}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeader;
