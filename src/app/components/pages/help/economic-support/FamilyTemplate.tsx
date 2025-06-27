c432371cf8281e67e5bd2d5548a5fccde16826c6
RalfiSlask <nilssonnarki@gmail.com>
2025-06-27 11:18:54 +0200

    feat: update and add template files for proxy and family

--- src/app/components/pages/help/economic-support/FamilyTemplate.tsx
+++ src/app/components/pages/help/economic-support/FamilyTemplate.tsx
@@ -1,37 +1,39 @@
 'use client';
 
 import SectionTitle from '@/app/components/shared/headings/SectionTitle';
-import { PROXY_TEMPLATE_TEXT } from '@/app/data/help/economic';
+import { FAMILY_PLAN_TEMPLATE_TEXT } from '@/app/data/help/economic';
 import { useState } from 'react';
 import { FaFileAlt } from 'react-icons/fa';
 import TemplateDownloadButtons from './TemplateDownloadButtons';
 import TemplatePreview from './TemplatePreview';
 import TemplatePreviewButton from './TemplatePreviewButton';
 
-const ProxyTemplate = () => {
+const FamilyTemplate = () => {
   const [showPreview, setShowPreview] = useState(false);
 
   const handleClickOnPreviewButton = () => {
     setShowPreview(!showPreview);
   };
 
-  const handleProxyDownload = (format: 'txt' | 'pdf' | 'docx') => {
+  const handleFamilyPlanDownload = (format: 'txt' | 'pdf' | 'docx') => {
     let url: string;
     let filename: string;
 
     switch (format) {
       case 'txt':
-        const blob = new Blob([PROXY_TEMPLATE_TEXT], { type: 'text/plain' });
+        const blob = new Blob([FAMILY_PLAN_TEMPLATE_TEXT], {
+          type: 'text/plain',
+        });
         url = window.URL.createObjectURL(blob);
-        filename = 'fullmakt-mall.txt';
+        filename = 'familjeplan-mall.txt';
         break;
       case 'pdf':
-        url = '/pdfs/fullmakt-mall.pdf';
-        filename = 'fullmakt-mall.pdf';
+        url = '/pdfs/familjeplan-mall.pdf';
+        filename = 'familjeplan-mall.pdf';
         break;
       case 'docx':
-        url = '/word/fullmakt-mall.docx';
-        filename = 'fullmakt-mall.docx';
+        url = '/word/familjeplan-mall.docx';
+        filename = 'familjeplan-mall.docx';
         break;
     }
 
@@ -47,19 +49,17 @@ const ProxyTemplate = () => {
 
     document.body.removeChild(linkElement);
   };
-
   return (
     <div className="content-container lightest-list-container">
-      <SectionTitle icon={<FaFileAlt />}>Mall för fullmakt</SectionTitle>
+      <SectionTitle icon={<FaFileAlt />}> Mall för familjeplan</SectionTitle>
       <div className="flex flex-col md:flex-row gap-4 md:gap-6 mb-4 md:mb-6">
         <p className="flex-1">
-          Här kan du ladda ner en mall för en enkel fullmakt som du kan anpassa
-          efter dina behov. Kom ihåg att kontrollera med din bank vad som krävs
-          specifikt. Kontrollera med din bank vad som krävs specifikt. Kom ihåg
-          att du när som helst kan återkalla fullmakten.
+          Här kan du ladda ner en mall för en familjeplan som du kan anpassa
+          efter dina behov. Denna plan hjälper dig och dina anhöriga att
+          förbereda för ekonomiska utmaningar vid skov.
         </p>
         <div className="flex flex-1 md:justify-center">
-          <TemplateDownloadButtons handleDownload={handleProxyDownload} />
+          <TemplateDownloadButtons handleDownload={handleFamilyPlanDownload} />
         </div>
       </div>
       <div className={`flex flex-col ${showPreview ? 'gap-4' : 'gap-0'}`}>
@@ -68,7 +68,7 @@ const ProxyTemplate = () => {
           handleClick={handleClickOnPreviewButton}
         />
         <TemplatePreview
-          templateText={PROXY_TEMPLATE_TEXT}
+          templateText={FAMILY_PLAN_TEMPLATE_TEXT}
           showPreview={showPreview}
         />
       </div>
@@ -76,4 +76,4 @@ const ProxyTemplate = () => {
   );
 };
 
-export default ProxyTemplate;
+export default FamilyTemplate;
