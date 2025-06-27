d1bc4f845f3763b9a621c8c5bbae80180016af29
RalfiSlask <nilssonnarki@gmail.com>
2025-06-27 13:27:06 +0200

    feat: import new components

--- src/app/hjalp/ekonomisk-hjalp/page.tsx
+++ src/app/hjalp/ekonomisk-hjalp/page.tsx
@@ -5,9 +5,12 @@ import ProxySection from '@/app/components/pages/help/economic-support/ProxySect
 import ProxyTemplate from '@/app/components/pages/help/economic-support/ProxyTemplate';
 import SocialSupportSection from '@/app/components/pages/help/economic-support/SocialSupportSection';
 import TrusteeSection from '@/app/components/pages/help/economic-support/TrusteeSection';
+import ContactCardsContainer from '@/app/components/shared/ContactCardsContainer';
 import EncouragmentContainer from '@/app/components/shared/EncouragmentContainer';
+import { MediumImageWithText } from '@/app/components/shared/MediumImageWithText';
 import PageIntroContainer from '@/app/components/shared/PageIntroContainer';
 import RelatedLinks from '@/app/components/shared/RelatedLinks';
+import { ECONOMIC_CONTACTS } from '@/app/data/help/economic';
 import { ECONOMIC_HELP_INTRO } from '@/app/data/pageIntros';
 import { HELP_RELATED_LINKS } from '@/app/data/related/relatedContentLinks';
 
@@ -23,7 +26,13 @@ const EconomicHelpPage = () => {
         <TrusteeSection />
         <FamilyAgreementSection />
         <SocialSupportSection />
-
+        <MediumImageWithText
+          title="Budget- och skuldrådgivning"
+          description="Många kommuner erbjuder gratis budget- och skuldrådgivning. Detta kan vara särskilt värdefullt om du har svårt att hålla koll på din ekonomi eller har hamnat i ekonomiska svårigheter."
+          image="/images/help/economic/budget.webp"
+          imageAlt="En miniräknare, penna, ett par gem och en blomväxt ovanpå ett papper är placerad på ett bord"
+        />
+        <ContactCardsContainer contacts={ECONOMIC_CONTACTS} />
         <EncouragmentContainer
           text="Att ta proaktiva steg för att skydda din ekonomi är ett tecken på styrka och självinsikt. 
           Genom att planera i förväg och sätta upp rätt skyddsåtgärder kan du minska risken för 
