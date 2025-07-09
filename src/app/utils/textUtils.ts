ea793f47299d703c5aafc6adb8a0482718d8778e
RalfiSlask <nilssonnarki@gmail.com>
2025-07-09 10:09:00 +0200

    utils: add a text util to make sure we visually dont show any html tags in texts

--- src/app/utils/textUtils.ts
+++ src/app/utils/textUtils.ts
@@ -1,3 +1,7 @@
 export const shortenUrl = (url: string) => {
   return url.replace(/^https?:\/\//, '').replace(/^www\./, '');
 };
+
+export const stripHtmlTags = (input: string): string => {
+  return input.replace(/<[^>]*>/g, '');
+};
