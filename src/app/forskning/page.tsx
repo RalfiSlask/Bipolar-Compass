b1e332d647d123df4eb28d9e15b3e26022e0b25b
RalfiSlask <nilssonnarki@gmail.com>
2025-07-17 13:38:07 +0200

    feat: create reusable search button and place in science page

--- src/app/forskning/page.tsx
+++ src/app/forskning/page.tsx
@@ -33,6 +33,7 @@ import { IoFilter } from 'react-icons/io5';
 import ScienceAgesFilter from '../components/pages/science/ScienceAgeFilter';
 import ScienceModal from '../components/pages/science/ScienceModal';
 import CustomSelect from '../components/shared/CustomSelectDropdown';
+import SearchButton from '../components/shared/buttons/SearchButton';
 
 const apiKey = process.env.NEXT_PUBLIC_PUB_MED_API_KEY;
 const BASE_URL = process.env.NEXT_PUBLIC_PUB_MED_BASE_URL;
@@ -484,26 +485,7 @@ const ScienceArticles = () => {
                   onChange={(e) => setAdditionalSearch(e.target.value)}
                 />
               </div>
-              <button
-                className="px-8 py-3 bg-primary-dark hover:bg-secondary-dark text-white font-medium rounded-lg transition-colors duration-200 flex items-center justify-center gap-2"
-                type="submit"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-                Sök
-              </button>
+              <SearchButton />
             </form>
             <div className="mt-4 text-sm text-gray-500">
               Din sökning kommer att kombineras med &quot;bipolar disorder&quot;
@@ -790,7 +772,7 @@ const ScienceArticles = () => {
                     </div>
                   )}
                   {isLoading && (
-                    <div className="absolute inset-0 bg-white/80 z-10 flex items-center justify-center">
+                    <div className="lightbox-loading-container">
                       <Spinner />
                     </div>
                   )}
