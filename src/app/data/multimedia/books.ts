6c421d4f748ff442ed8f6b040ad840dd5d2217ec
RalfiSlask <nilssonnarki@gmail.com>
2025-07-17 13:37:23 +0200

    data: add book data

---
+++ src/app/data/multimedia/books.ts
@@ -0,0 +1,43 @@
+export const BOOK_CATEGORIES = [
+  {
+    id: 1,
+    name: 'Psychology',
+    label: 'Psykologi',
+  },
+  {
+    id: 2,
+    name: 'Medical',
+    label: 'Medicin',
+  },
+  {
+    id: 3,
+    name: 'Self-Help',
+    label: 'Självhjälp',
+  },
+  {
+    id: 4,
+    name: 'Health & Fitness',
+    label: 'Hälsa & Fitness',
+  },
+  {
+    id: 5,
+    name: 'Family & Relationships',
+    label: 'Familj & Relationer',
+  },
+  {
+    id: 6,
+    name: 'Education',
+    label: 'Utbildning',
+  },
+  {
+    id: 7,
+    name: 'Biography & Autobiography',
+    label: 'Biografi',
+  },
+];
+
+export const SORT_OPTIONS = [
+  { value: 'relevance', label: 'Relevans' },
+  { value: 'rating', label: 'Betyg' },
+  { value: 'newest', label: 'Nyast' },
+];
