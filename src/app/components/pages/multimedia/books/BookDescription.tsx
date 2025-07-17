d0c81e341977fdd7e008540f68a0379c790797a4
RalfiSlask <nilssonnarki@gmail.com>
2025-07-17 13:40:48 +0200

    styles: update ui of the book description

--- src/app/components/pages/multimedia/books/BookDescription.tsx
+++ src/app/components/pages/multimedia/books/BookDescription.tsx
@@ -2,11 +2,9 @@ import { stripHtmlTags } from '@/app/utils/textUtils';
 
 const BookDescription = ({ description }: { description: string }) => {
   return (
-    <div>
-      <h3 className="font-medium mb-2">Beskrivning</h3>
-      <p className="text-gray-600 whitespace-pre-line">
-        {stripHtmlTags(description)}
-      </p>
+    <div className="border-t border-primary-dark/30 pt-4">
+      <h3 className="font-semibold mb-2 text-primary-dark">Beskrivning</h3>
+      <p className="whitespace-pre-line">{stripHtmlTags(description)}</p>
     </div>
   );
 };
